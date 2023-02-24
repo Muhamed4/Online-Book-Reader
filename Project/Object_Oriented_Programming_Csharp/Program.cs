c2cef180a2abf40bce9aacdbdb8dcbecff9059e2
mohamed_morsi <mohamedmorsi722@gmail.com>
2023-02-24 20:15:28 +0200

    Initialize the project

---
+++ Project/Object_Oriented_Programming_Csharp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Project
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
+
